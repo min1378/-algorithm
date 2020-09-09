69062dc840f08dff7bf4a225363729851ae7dfc2
min1378 <qwes123@naver.com>
2020-09-09 23:57:25 +0900

    무지의 먹방

--- kakao/42587_기능개발.js
+++ kakao/42587_기능개발.js
@@ -1,13 +1,14 @@
 function solution(progresses, speeds) {
   let endDays = []
   for (let i = 0; i < progresses.length; i++) {
-    let result = parseInt((100 - progresses[i]) / speeds[i])
-    if ((100 - progresses[i]) % speeds[i]) result++
+    let result = parseInt((100 - progresses[i]) / speeds[i]) // 몫
+    if ((100 - progresses[i]) % speeds[i]) result++ // 나머지가 있을때 
     endDays.push(result)
   }
   let answer = []
   let count = 0
   let standard = endDays[0]
+  console.log(endDays)
   for (const endDay of endDays) {
     if (standard < endDay) {
       standard = endDay
---
+++ kakao/42891_무지의먹방.js
@@ -0,0 +1,45 @@
+// function solution(food_times, k) {
+//   var answer = 0
+//   let sortArray = food_times
+//     .map((e, i) => {
+//       return { time: e, index: i + 1 }
+//     })
+//     .sort((a, b) => {
+//       return a.time - b.time
+//     })
+//   console.log(sortArray)
+//   return answer
+// }
+function solution(food_times, k) {
+  var answer = 0
+  var i = 0
+  var t = 0
+  const order = food_times
+    .map((time, index) => {
+      return { time, index }
+    })
+    .sort((a, b) => {
+      if (a.time > b.time) return 1
+      if (a.time < b.time) return -1
+      if (a.index > b.index) return 1
+      if (a.index < b.index) return -1
+      return 0
+    })
+  while (k >= (order.length - i) * (order[i].time - t)) {
+    k -= (order.length - i) * (order[i].time - t)
+    t = order[i].time
+    while (order[i].time - t <= 0) {
+      order[i].time -= t
+      if (order[++i] === undefined) return -1
+    }
+  }
+  k = k % (order.length - i)
+  for (let j = 0; j < i; j++) {
+    if (order[j].index <= k) k++
+  }
+  return k + 1
+}
+
+for (let i = 1; i < 12; i++) {
+  console.log(i, solution([4, 1, 1, 5], 10))
+}
